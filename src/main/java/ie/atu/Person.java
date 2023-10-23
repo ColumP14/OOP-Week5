c18ab5d1effb7ff4547c13d3d1be13a076dcfab9
Colum Peters <g00427223@atu.ie>
2023-10-23 16:46:16 +0100

    Added method to getUserInput for names and age. Added method to display user input data.

--- src/main/java/ie/atu/Person.java
+++ src/main/java/ie/atu/Person.java
@@ -11,13 +11,33 @@ public class Person {
         this.lastName = "";
         this.age = 0;
     }
-//Constructor
+    //Constructor
     public Person(String firstName, String lastName, int age) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.age = age;
     }
 
+    //Method to take user input
+    public void getUserInput(){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter first Name: ");
+        this.firstName = scanner.nextLine();
+
+        System.out.println("Enter Last Name: ");
+        this.lastName = scanner.nextLine();
+
+        System.out.println("Enter Age: ");
+        this.age = scanner.nextInt();
+    }
+
+    //Method to display info
+    public void displayInfo(){
+        System.out.println("Name: " + firstName);
+        System.out.println("Last Name: " + lastName);
+        System.out.println("Age: " + age);
+    }
     public String getFirstName() {
         return firstName;
     }
