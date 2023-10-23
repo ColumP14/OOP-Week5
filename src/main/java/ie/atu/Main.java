dc792afb4b1cbabcfbb2414f1dfbb35b4e0c37b2
Colum Peters <g00427223@atu.ie>
2023-10-23 16:39:06 +0100

    Added getter and setter methods to Person.java

--- src/main/java/ie/atu/Main.java
+++ src/main/java/ie/atu/Main.java
@@ -5,5 +5,6 @@ package ie.atu;
 public class Main {
     public static void main(String[] args) {
         Person student1 = new Person("Colum","Peters", 20);
+        System.out.println("Details are " + student1.getFirstName() + " " + student1.getLastName() + ", " + "age " + student1.getAge());
     }
 }
--- src/main/java/ie/atu/Person.java
+++ src/main/java/ie/atu/Person.java
@@ -1,7 +1,7 @@
 package ie.atu;
+
+import java.util.Scanner;
 public class Person {
-
-
     private String firstName;
     private String lastName;
     private int age;
@@ -11,10 +11,34 @@ public class Person {
         this.lastName = "";
         this.age = 0;
     }
-
+//Constructor
     public Person(String firstName, String lastName, int age) {
         this.firstName = firstName;
         this.lastName = lastName;
+        this.age = age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
         this.age = age;
     }
 }
